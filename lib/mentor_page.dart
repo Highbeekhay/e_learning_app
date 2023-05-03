6c62f628cf4c48a1ea6e89341a5149553aa0b0a0
Highbeekhay <118852889+Highbeekhay@users.noreply.github.com>
2023-05-03 13:59:41 +0100

    profile 1

--- lib/mentor_page.dart
+++ lib/mentor_page.dart
@@ -11,91 +11,282 @@ class _MentorPageState extends State<MentorPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.fromLTRB(10, 8, 10, 8),
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade100,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Icon(
-                    Icons.arrow_back_ios_sharp,
-                    color: Colors.indigo,
-                    size: 20,
-                  ),
-                ),
-                SizedBox(
-                  width: 120,
-                ),
-                Text(
-                  "Mentor Profile",
-                  style: TextStyle(
-                    color: Colors.indigo,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Center(
-              child: Column(
+      body: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        child: Container(
+          padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
                 children: [
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      padding: const MaterialStatePropertyAll(
-                        EdgeInsets.all(50),
-                      ),
-                      overlayColor: const MaterialStatePropertyAll(
-                        Colors.white,
-                      ),
-                      shape: const MaterialStatePropertyAll(
-                        CircleBorder(),
-                      ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(10, 8, 10, 8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    onPressed: () {},
-                    child: Text(
-                      'JP',
-                      style: TextStyle(
-                          color: Colors.blue.shade200,
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold),
+                    child: Icon(
+                      Icons.arrow_back_ios_sharp,
+                      color: Colors.indigo,
+                      size: 20,
                     ),
                   ),
                   SizedBox(
-                    height: 30,
+                    width: 120,
                   ),
                   Text(
-                    'Joseph Grover',
+                    "Mentor Profile",
                     style: TextStyle(
-                      color: Color.fromARGB(255, 27, 63, 116),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Text(
-                    'Professor of Biotechnology',
-                    style: TextStyle(
-                      color: Colors.black38,
-                      fontWeight: FontWeight.bold,
+                      color: Colors.indigo,
                       fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
-                  )
+                  ),
                 ],
               ),
-            ),
-          ],
+              SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        padding: const MaterialStatePropertyAll(
+                          EdgeInsets.all(50),
+                        ),
+                        overlayColor: const MaterialStatePropertyAll(
+                          Colors.white,
+                        ),
+                        shape: const MaterialStatePropertyAll(
+                          CircleBorder(),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'JP',
+                        style: TextStyle(
+                            color: Colors.blue.shade200,
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      'Joseph Grover',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 27, 63, 116),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      'Professor of Biotechnology',
+                      style: TextStyle(
+                        color: Colors.black38,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                            padding: const EdgeInsets.fromLTRB(21, 9, 21, 8),
+                            decoration: BoxDecoration(
+                              color: Colors.indigo.shade50,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Text(
+                              "f",
+                              style: TextStyle(
+                                color: Colors.indigo,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                          decoration: BoxDecoration(
+                            color: Colors.indigo.shade50,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Icon(
+                            Icons.photo_camera_outlined,
+                            color: Colors.indigo,
+                            size: 20,
+                          ),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.fromLTRB(17, 11, 17, 11),
+                            decoration: BoxDecoration(
+                              color: Colors.indigo.shade50,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Text(
+                              "in",
+                              style: TextStyle(
+                                color: Colors.indigo,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+                          decoration: BoxDecoration(
+                            color: Colors.indigo.shade50,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Icon(
+                            Icons.supervised_user_circle_outlined,
+                            color: Colors.indigo,
+                            size: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                          ),
+                          child: Column(
+                            children: [
+                              Text(
+                                "Students",
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black38,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '700+',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 27, 63, 116),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(25, 20, 25, 20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                          ),
+                          child: Column(
+                            children: [
+                              Text(
+                                "Courses",
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black38,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '25+',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 27, 63, 116),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                          ),
+                          child: Column(
+                            children: [
+                              Text(
+                                "Rating",
+                                style: TextStyle(
+                                  letterSpacing: 1,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black38,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "5.0",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 27, 63, 116),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                "About",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 27, 63, 116),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: 462,
+                ),
+                child: Text(
+                  'Joseph Grover receieved his B.S. in Biomedical Engineering from Washington University in St. Louis in 2006 and his PhD in Bio...Read More',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
